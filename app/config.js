275565f5d2213ce2a49b941fc6bba0bc20e2f9c5
James Medd <james.medd@mac7509.bjss.co.uk>
2022-10-31 10:05:05 +0000

    Begin work on basic pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Apply for support with your energy bills',
 
   // Default port that prototype runs on
   port: '3000',
