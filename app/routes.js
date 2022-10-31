af76cf3370f049eae98ec8765fece931669bcdae
James Medd <james.medd@mac7509.bjss.co.uk>
2022-10-31 14:55:26 +0000

    Start work on happy path

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,53 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+const afpOptions = ['gas', 'oil', 'wood', 'solid-fuel', 'heat-network']
+
+router.get(['/afp-check'], (req, res) => {
+  if (afpOptions.some(option => option === req.session.data['central-heating-type'])) {
+    res.redirect('/afp-route')
+  } else {
+    res.redirect('/applying-for-yourself-or-someone-else')
+  }
+})
+
+router.get(['/third-party-check'], (req, res) => {
+  if (req.session.data['applying-for-yourself-or-someone-else'] === 'myself') {
+    res.redirect('/what-is-your-full-name')
+  } else {
+    res.redirect('/third-party-route')
+  }
+})
+
+router.get(['/residence-check'], (req, res) => {
+  if (req.session.data['is-this-your-main-residence'] === 'yes') {
+    res.redirect('/does-your-household-pay-council-tax')
+  } else {
+    res.redirect('/ineligible')
+  }
+})
+
+router.get(['/council-tax-check'], (req, res) => {
+  if (req.session.data['does-your-household-pay-council-tax'] === 'yes') {
+    res.redirect('/is-your-name-on-the-council-tax-bill')
+  } else {
+    res.redirect('/do-you-have-any-proof-of-address')
+  }
+})
+
+router.get(['/name-on-bill-check'], (req, res) => {
+  if (req.session.data['is-your-name-on-the-council-tax-bill'] === 'yes') {
+    res.redirect('/do-you-have-a-bank-account')
+  } else {
+    res.redirect('/do-you-have-any-proof-of-address')
+  }
+})
+
+router.get(['/bank-account-check'], (req, res) => {
+  if (req.session.data['do-you-have-a-bank-account'] === 'yes') {
+    res.redirect('/what-are-your-bank-account-details')
+  } else {
+    res.redirect('/how-would-you-like-to-be-paid')
+  }
+})
